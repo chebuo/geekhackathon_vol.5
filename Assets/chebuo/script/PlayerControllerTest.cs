a5d82271ca673da887e35837134e3e5e9f33daab
wasijyayo <meganekaitai@gmail.com>
2025-06-22 09:15:13 +0900

    [add]スキーの挙動修正。sticのアニメション追加

--- Assets/chebuo/script/PlayerControllerTest.cs
+++ Assets/chebuo/script/PlayerControllerTest.cs
@@ -8,6 +8,8 @@ public class PlayerControllerTest : MonoBehaviour
     public static bool isGame = false;
     public static bool isGoal = false;
     Rigidbody rb;
+    private int rotation_sub=80;
+    private int rotation_rimit = 40;
 
     void Start()
     {
@@ -30,28 +32,27 @@ public class PlayerControllerTest : MonoBehaviour
         Debug.Log("stop: " + UDPSensorReceiver.stop);
         // Debug.Log("isJump: " + UDPSensorReceiver.isJump); // •K—v‚É‰ž‚¶‚Ä
 
-        rb.AddForce(Vector3.back * -5);
+        //rb.AddForce(this.transform.forward * 5);
         if (rb.velocity.magnitude > 40)
         {
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, 40);
         }
 
-        rb.AddForce(Vector3.back * UDPSensorReceiver.balance_x / 4);
-        rb.AddForce(Vector3.back * UDPSensorReceiver.balance_x / 4);
-        rb.AddForce(Vector3.right * UDPSensorReceiver.balance_z / 4);
-        rb.AddForce(Vector3.right * UDPSensorReceiver.balance_z / 4);
+        rb.AddForce(this.transform.forward * -UDPSensorReceiver.balance_x / 4);
+
+        // ‰¡•ûŒü‚ÌˆÚ“®‚ðíœ‚µAŠp“x‚É”½‰f
+        Transform transform = this.transform;
+        Vector3 worldAngle = transform.eulerAngles;
+
+        if (Mathf.Abs(-UDPSensorReceiver.balance_z) > rotation_rimit)
+        {
+            worldAngle.y -= -UDPSensorReceiver.balance_z / rotation_sub;
+            transform.eulerAngles = worldAngle;
+        }
 
         if (UDPSensorReceiver.stop)
         {
             rb.AddForce(Vector3.zero);
-        }
-
-        Transform transform = this.transform;
-        Vector3 worldAngle = transform.eulerAngles;
-        if (Mathf.Abs(UDPSensorReceiver.balance_z) < 60)
-        {
-            worldAngle.y = UDPSensorReceiver.balance_z;
-            transform.eulerAngles = worldAngle;
         }
     }
 
